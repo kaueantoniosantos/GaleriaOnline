19f8e56bcf1582fe7ab8d95bc0f14e9458360010
Kauê Santos <kaueantoniofranciscosantos@gmail.com>
2025-08-25 22:58:55 -0300

    feat: inserindo comentários para explicar as rotas

--- GaleriaOnlineReact/src/App.jsx
+++ GaleriaOnlineReact/src/App.jsx
@@ -7,9 +7,13 @@ function App() {
 
   return (
     <>
+    {/* o routes pode ser entendido como uma "div" para as rotas
+    o route é uma tag específica que roda a página através do elemento inserido na url, sendo esse elemento o mesmo inserido entre aspas */}
       <Routes>
         <Route path="/" element={<Home/>} />
+        {/* aqui ele mostra que quando você colocar esse elemento dentro das aspas na url ele vai ir para tal página */}
         <Route path="/galeria" element={<Galeria/>} />
+        {/* aqui ele mostra que quando você colocar esse elemento dentro das aspas na url ele vai ir para tal página */}
       </Routes>
     </>
   )
